66da1cc0d0a95dcdb7c2e88e959302ac52e9648e
Seungkyu-Han <>
2023-07-23 01:07:04 +0900

    [feat] findTop6ByOrderByCreatedAtDesc 추가

--- src/main/java/cobo/blog/global/Repository/ProjectRepository.java
+++ src/main/java/cobo/blog/global/Repository/ProjectRepository.java
@@ -4,8 +4,11 @@ import cobo.blog.global.Data.Entity.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Integer> {
 
+    List<ProjectEntity> findTop6ByOrderByCreatedAtDesc();
 
 }
