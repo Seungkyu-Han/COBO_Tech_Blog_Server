2c334451d5cb77042c77cc5bcffb5979924e306e
Seungkyu-Han <>
2023-07-24 15:49:21 +0900

    [refactor] get Post 페이징으로 구현

--- src/main/java/cobo/blog/domain/Tech/TechController.java
+++ src/main/java/cobo/blog/domain/Tech/TechController.java
@@ -1,14 +1,12 @@
 package cobo.blog.domain.Tech;
 
 import cobo.blog.domain.Tech.Data.Dto.TechPostRes;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import io.swagger.annotations.*;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -24,14 +22,19 @@ public class TechController {
     @GetMapping("/posts")
     @ApiOperation(
             value = "Tech Post 의 정보를 List 응답",
-            notes = "현재 전부 넘기고 있어서 몇페이지로 자를 건지 협의가 필요",
-            response = TechPostRes.class,
-            produces = "application/json"
+            notes = "페이지 1부터 시작합니다, 주의 부탁드립니다.",
+            response = TechPostRes.class
     )
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "페이지 번호", example = "1", required = true),
+            @ApiImplicitParam(name = "size", value = "페이지의 사이즈", example = "10", required = true)
+    })
     @ApiResponses({
             @ApiResponse(code = 200, message = "응답 성공")
     })
-    public ResponseEntity<List<TechPostRes>> getPosts(){
-        return techService.getPosts();
+    public ResponseEntity<List<TechPostRes>> getPosts(
+            @RequestParam("page") Integer page,
+            @RequestParam("size") Integer size){
+        return techService.getPosts(page, size);
     }
 }
