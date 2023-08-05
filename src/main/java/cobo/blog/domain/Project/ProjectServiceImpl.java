2192269f27272cd266f61736a9862b3a15991ea5
Seungkyu-Han <>
2023-08-05 22:03:53 +0900

    [feat] 프로젝트 카드에 페이징 추가

--- src/main/java/cobo/blog/domain/Project/ProjectServiceImpl.java
+++ src/main/java/cobo/blog/domain/Project/ProjectServiceImpl.java
@@ -4,6 +4,9 @@ import cobo.blog.domain.Project.Data.Dto.ProjectProjectCardRes;
 import cobo.blog.global.Data.Entity.ProjectEntity;
 import cobo.blog.global.Repository.ProjectRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -17,10 +20,16 @@ public class ProjectServiceImpl {
 
     private final ProjectRepository projectRepository;
 
-    public ResponseEntity<List<ProjectProjectCardRes>> getProjectCards() {
+    public ResponseEntity<List<ProjectProjectCardRes>> getProjectCards(Integer page, Integer size) {
         List<ProjectProjectCardRes> projectProjectCardRes = new ArrayList<>();
-        for(ProjectEntity projectEntity : projectRepository.findAll())
+        for(ProjectEntity projectEntity : this.getProjectEntityWithPaging(page, size))
             projectProjectCardRes.add(new ProjectProjectCardRes(projectEntity));
         return new ResponseEntity<>(projectProjectCardRes, HttpStatus.OK);
+    }
+
+    public Page<ProjectEntity> getProjectEntityWithPaging(int page, int size){
+        return projectRepository.findAll(PageRequest.of(
+                page - 1, size, Sort.by(Sort.Direction.DESC, "id")
+                ));
     }
 }
