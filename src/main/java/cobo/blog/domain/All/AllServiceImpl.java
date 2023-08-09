8cb748eef801dc77d6678164add87a46a2b0edaa
Seungkyu-Han <>
2023-08-09 23:03:35 +0900

    [refactor] ip log 삭제

--- src/main/java/cobo/blog/domain/All/AllServiceImpl.java
+++ src/main/java/cobo/blog/domain/All/AllServiceImpl.java
@@ -24,9 +24,6 @@ public class AllServiceImpl {
     @Transactional
     public ResponseEntity<AllHitRes> getHit(Integer hitCookie, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
 
-        log.info("Hit API 호출");
-        log.info("요청한 IP : {}", getIp(httpServletRequest));
-
         Long today = Long.parseLong(Objects.requireNonNull(redisTemplate.opsForValue().get("today")));
         Long total = Long.parseLong(Objects.requireNonNull(redisTemplate.opsForValue().get("total")));
 
@@ -35,38 +32,11 @@ public class AllServiceImpl {
         return new ResponseEntity<>(new AllHitRes(today, today + total), HttpStatus.OK);
     }
 
-    private String getIp(HttpServletRequest request) {
-        String ipAddress = request.getHeader("X-Forwarded-For");
-
-        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("Proxy-Client-IP");
-        }
-
-        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("WL-Proxy-Client-IP");
-        }
-
-        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("HTTP_CLIENT_IP");
-        }
-
-        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
-        }
-
-        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
-            ipAddress = request.getRemoteAddr();
-        }
-
-        return ipAddress;
-    }
-
     private void IncrementTodayAndSetCookie(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
         redisTemplate.opsForValue().increment("today");
 
         Cookie cookie = new Cookie("hitCookie", "1");
         cookie.setMaxAge(900);
         httpServletResponse.addCookie(cookie);
-        log.info("{} IP 조회수 + 1", httpServletRequest.getHeader("X-Forwarded-For"));
     }
 }
