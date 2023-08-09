8b2f4eb3c53da2768cfd1759d78cdcc13ba12da5
Seungkyu-Han <>
2023-08-09 16:36:31 +0900

    [refactor] viewCount log 추가

--- src/main/java/cobo/blog/domain/All/AllServiceImpl.java
+++ src/main/java/cobo/blog/domain/All/AllServiceImpl.java
@@ -25,7 +25,7 @@ public class AllServiceImpl {
     public ResponseEntity<AllHitRes> getHit(Integer hitCookie, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
 
         log.info("Hit API 호출");
-        log.info("요청한 IP : {}", httpServletRequest.getHeader("X-Forwarded-For"));
+        log.info("요청한 IP : {}", getIp(httpServletRequest));
 
         Long today = Long.parseLong(Objects.requireNonNull(redisTemplate.opsForValue().get("today")));
         Long total = Long.parseLong(Objects.requireNonNull(redisTemplate.opsForValue().get("total")));
@@ -33,6 +33,32 @@ public class AllServiceImpl {
         if(hitCookie == 0) IncrementTodayAndSetCookie(httpServletRequest, httpServletResponse);
 
         return new ResponseEntity<>(new AllHitRes(today, today + total), HttpStatus.OK);
+    }
+
+    private String getIp(HttpServletRequest request) {
+        String ipAddress = request.getHeader("X-Forwarded-For");
+
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_CLIENT_IP");
+        }
+
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("HTTP_X_FORWARDED_FOR");
+        }
+
+        if (ipAddress == null || ipAddress.isEmpty() || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+        }
+
+        return ipAddress;
     }
 
     private void IncrementTodayAndSetCookie(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse){
