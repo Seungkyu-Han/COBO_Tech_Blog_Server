4f7b3f3f8b7578dc5acb8ef4378a433749c46a44
Seungkyu-Han <>
2023-08-10 16:49:42 +0900

    [feat] SkillTag 조회 API 추가

--- src/main/java/cobo/blog/global/Repository/SkillTagRepository.java
+++ src/main/java/cobo/blog/global/Repository/SkillTagRepository.java
@@ -2,8 +2,15 @@ package cobo.blog.global.Repository;
 
 import cobo.blog.global.Data.Entity.SkillTagEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SkillTagRepository extends JpaRepository<SkillTagEntity, Integer> {
+    @Query(
+            "SELECT e.name FROM SkillTagEntity e"
+    )
+    List<String> getNameOfSkillTags();
 }
