1dc7658700be11adea0c4f809a838fb4dc85f625
Seungkyu-Han <>
2023-08-22 13:24:33 +0900

    [feat] 모든 헤더 허용

--- src/main/java/cobo/blog/global/Config/WebConfig.java
+++ src/main/java/cobo/blog/global/Config/WebConfig.java
@@ -13,6 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
+                .allowedHeaders("*")
                 .maxAge(6000);
     }
 }
