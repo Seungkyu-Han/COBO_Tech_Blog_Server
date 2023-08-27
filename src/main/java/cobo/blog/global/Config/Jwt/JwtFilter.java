9ef928a0953f12d9c3620e03954d137f0670bcca
Seungkyu-Han <>
2023-08-27 11:27:25 +0900

    [fix] get post List 에러 fix

--- src/main/java/cobo/blog/global/Config/Jwt/JwtFilter.java
+++ src/main/java/cobo/blog/global/Config/Jwt/JwtFilter.java
@@ -31,7 +31,7 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 
-        if(!isAuthPath(request.getServletPath())){
+        if(!isAuthPath(request.getServletPath()) || (request.getMethod().equals("GET") && request.getServletPath().startsWith("/api/tech/post"))){
             filterChain.doFilter(request, response);
             return;
         }
